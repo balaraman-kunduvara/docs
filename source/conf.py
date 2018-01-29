1deec71484df43a9ba2bf9cbdfc6f9cd118ba414
Kunduvara, Balaraman <Balaraman.Kunduvara@sc.com>
2018-01-29 19:24:56 +0800

    Changing back to alabaster

--- source/conf.py
+++ source/conf.py
@@ -84,16 +84,16 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "rightsidebar": "false",
-    "relbarbgcolor": "black"
-}
+#html_theme_options = {
+#    "rightsidebar": "false",
+#    "relbarbgcolor": "black"
+#}
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
